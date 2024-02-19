b8eaa00a33e4a7e8b8fb4fd8b15e57f3a6085650
vaniilajs <vaniillajs@gmail.com>
2024-02-19 10:12:17 +0530

    chore: Update Babel and Tailwind configurations and added ui components

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,30 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["nativewind/babel", "react-native-reanimated/plugin"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: {
+            "@": "./src",
+            "@env": "./src/core/env.js",
+          },
+          extensions: [
+            ".ios.ts",
+            ".android.ts",
+            ".ts",
+            ".ios.tsx",
+            ".android.tsx",
+            ".tsx",
+            ".jsx",
+            ".js",
+            ".json",
+          ],
+        },
+      ],
+      "nativewind/babel",
+      "react-native-reanimated/plugin",
+    ],
   };
 };
---
+++ src/ui/focus-aware-status-bar.tsx
@@ -0,0 +1,9 @@
+import { useIsFocused } from "@react-navigation/native";
+import * as React from "react";
+import { StatusBar } from "react-native";
+
+type Props = React.ComponentProps<typeof StatusBar>;
+export const FocusAwareStatusBar = (props: Props) => {
+  const isFocused = useIsFocused();
+  return isFocused ? <StatusBar barStyle={"dark-content"} {...props} /> : null;
+};
