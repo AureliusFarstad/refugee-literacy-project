577eb869ec1d1750bc42a74a146a49b4583cb93d
vaniilajs <vaniillajs@gmail.com>
2024-02-19 10:01:03 +0530

    feat: transitioned from app to src directory, added typescript support and env

--- app/config/colors.js
+++
@@ -1,9 +0,0 @@
-export default {
-  primary: "#fc5c65",
-  secondary: "#4ecdc4",
-  background: "#EDE5E0",
-  speaker: "#9CD6DF",
-  black: "#000",
-  icons: "#9CD6DF",
-  border: "#AEAAA8",
-};
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import "react-native-gesture-handler";
+
+import { Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+export default App;
