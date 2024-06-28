a904361dab8fcaa9d6abd8fa76cc6a01b8c5dee7
vaniilajs <vaniillajs@gmail.com>
2024-06-28 12:08:29 +0530

    comments added for get options to render function and will return only two elements at a time

--- src/utils/level-one/index.ts
+++ src/utils/level-one/index.ts
@@ -1,16 +1,39 @@
 /**
  * 1. shuffle the options available
- * 2. pick 3 of them and add the correct answer at the end,
+ * 2. pick 2 of them and adds the correct answer at the end,
  * 3. shuffle the options again
  */
 
+/**
+input
+ [
+  {"id": "optionA", "title": "A"},
+  {"id": "optionT", "title": "T"},
+  {"id": "optionP", "title": "P"},
+  {"id": "optionI", "title": "I"},
+  {"id": "optionN", "title": "N"}
+]
+  and
+
+correctAnswer {"id": "optionS", "title": "S"}
+
+output
+
+[
+{"id": "optionA", "title": "A"},
+{"id": "optionT", "title": "T"},
+{"id": "optionS", "title": "S"}
+]
+
+
+*/
 export const getOptionsToRender = (
   options: IOption[],
   _correctAnswer: IOption
 ): IOption[] => {
   const shuffledOptions = options.sort(() => Math.random() - 0.5);
   const _shuffledOptionsWithCorrectAnswer = [
-    ...shuffledOptions.slice(0, 3),
+    ...shuffledOptions.slice(0, 2),
     _correctAnswer,
   ].sort(() => Math.random() - 0.5);
   return _shuffledOptionsWithCorrectAnswer;
