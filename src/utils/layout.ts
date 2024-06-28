a2b17305e2c1a49a9560f84e929fc4afa11034e6
vaniilajs <vaniillajs@gmail.com>
2024-06-28 12:06:22 +0530

    isIos isAndroid utils added

--- src/utils/layout.ts
+++ src/utils/layout.ts
@@ -1,7 +1,10 @@
-import { Dimensions, Platform } from "react-native"
+import { Dimensions, Platform } from "react-native";
 
-export const IS_IOS = Platform.OS === "ios"
-const { width, height } = Dimensions.get("screen")
+export const IS_IOS = Platform.OS === "ios";
+const { width, height } = Dimensions.get("screen");
 
-export const WIDTH = width
-export const HEIGHT = height
+export const WIDTH = width;
+export const HEIGHT = height;
+
+export const isIos = Platform.OS === "ios";
+export const isAndroid = Platform.OS === "android";
