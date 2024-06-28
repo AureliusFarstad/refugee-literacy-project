8f23159e9039551f8ae68a2e6953ac0d8123cb62
vaniilajs <vaniillajs@gmail.com>
2024-06-28 12:07:51 +0530

    feat: letter name activity added

--- src/app/(level-one)/letter-name.tsx
+++ src/app/(level-one)/letter-name.tsx
@@ -1,23 +1,252 @@
-import { SafeAreaView, Text, View } from "@/ui";
+import { useLevelStore } from "@/core/store/levels";
+import { Pressable, SafeAreaView, Text, View } from "@/ui";
+import { Switch } from "@/ui/checkbox";
 import Header from "@/ui/core/headers";
 import { DynamicModal } from "@/ui/core/modal/dynamic-modal";
-import { NameIcon } from "@/ui/icons";
-import React, { useRef } from "react";
+import { EarIcon } from "@/ui/icons";
+import { getOptionsToRender } from "@/utils/level-one";
+import clsx from "clsx";
+import { Audio } from "expo-av";
+import { Sound } from "expo-av/build/Audio";
+import { Href, router } from "expo-router";
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import { Alert, ToastAndroid } from "react-native";
+
+const SwitchExample = () => {
+  const [active, setActive] = React.useState(false);
+  return (
+    <Switch.Root
+      checked={active}
+      onChange={setActive}
+      accessibilityLabel="switch"
+      className="pb-2"
+    >
+      <Switch.Icon checked={active} />
+      <Switch.Label text="" />
+    </Switch.Root>
+  );
+};
 
 const LetterName = () => {
   const dynamicModalRef = useRef<DynamicModalRefType>(null);
+  const { levels, updateLevels } = useLevelStore();
+  const [sound, setSound] = useState<Sound>();
+  const [isUpdatingSession, setIsUpdatingSession] = useState(false);
+  const [tappedAnswer, setTappedAnswer] = useState<IOption>();
+
+  const [activeActivity, setActiveActivity] =
+    useState<IActivityWithSoundAndName>(
+      levels[0].modules[0].sections[3].activities[0]
+    );
+
+  const optionsToRender = useMemo(
+    () =>
+      getOptionsToRender(activeActivity.options, activeActivity?.correctAnswer),
+    [activeActivity]
+  );
+
+  /**
+   * 1. 6 letters, S,A,T,P,I,N
+   * 2. Save the progress
+   * 3. One by one do the mapping, show 3 of the rest of the five (have the option to show rest of 25 apart from the correct one)
+   * 4. Correct answer, show success
+   * 5. Wrong answer, show try again
+   * 6. Level completion UI
+   *
+   */
+
+  const playSound = async () => {
+    try {
+      const { sound } = await Audio.Sound.createAsync(activeActivity.audio);
+      if (sound) {
+        setSound(sound);
+      }
+      console.log("Playing Sound");
+      await sound.playAsync();
+    } catch (error) {
+      console.log("error in playSound", error);
+      throw error;
+    }
+  };
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
+  const initNextActivity = () => {
+    const currentIndex = levels[0].modules[0].sections[3].activities.findIndex(
+      (activity: IActivity) => activity.id === activeActivity.id
+    );
+    let _nextActivity: IActivity;
+    if (
+      currentIndex === -1 ||
+      currentIndex === levels[0].modules[0].sections[3].activities.length - 1
+    ) {
+      // If current element is not found or is the last element, return the first element
+      _nextActivity = levels[0].modules[0].sections[3].activities[0];
+    } else {
+      // Return the next element in the array
+      _nextActivity =
+        levels[0].modules[0].sections[3].activities[currentIndex + 1];
+    }
+
+    if (_nextActivity) {
+      setActiveActivity(_nextActivity);
+    }
+  };
+
+  useEffect(() => {
+    /**
+     * Check if each activity have been answered correctly twice if so means level completed
+     */
+    const currentSection = levels[0].modules[0].sections[3];
+    if (
+      currentSection.activities.every(
+        (activity) => activity.numberOfTimesCorrectAnswerGiven === 2
+      )
+    ) {
+      Alert.alert("Done");
+    }
+  });
 
   return (
     <SafeAreaView>
-      <Header title="Name" modalRef={dynamicModalRef} />
-      <View className="bg-[#ECE5E1] flex items-center p-4">
-        <Text>content goes here</Text>
+      <Header title="Sound" modalRef={dynamicModalRef} />
+      <View className="px-5 mt-5 bg-green-500">
+        <SwitchExample />
+      </View>
+      <View className="flex items-center p-4">
+        <Pressable
+          onPress={playSound}
+          className="bg-colors-purple-500 w-[110] h-[110] rounded-full flex items-center justify-center"
+        >
+          <EarIcon />
+        </Pressable>
+        <View className="absolute top-0 flex flex-row w-full">
+          <Text>{activeActivity.numberOfTimesCorrectAnswerGiven}</Text>
+          <Text>{activeActivity.correctAnswer.title}</Text>
+        </View>
+        <View className="flex flex-row w-full flex-1">
+          {optionsToRender.map((option, index) => (
+            <Pressable
+              key={option.id}
+              onPress={() => {
+                setTappedAnswer(option);
+                if (option.id === activeActivity.correctAnswer.id) {
+                  console.log("correct answer");
+
+                  const _updatedLevels = levels.map((level: ILevel) => {
+                    if (level.id !== levels[0].id) return level;
+
+                    const _updatedModules = level.modules.map((sublevel) => {
+                      if (sublevel.id !== levels[0].modules[0].id)
+                        return sublevel;
+
+                      const _updatedSections = sublevel.sections.map(
+                        (section: ISection) => {
+                          if (
+                            section.id !== levels[0].modules[0].sections[3].id
+                          )
+                            return section;
+
+                          const _updatedActivities = section.activities.map(
+                            (activity: IActivity) => {
+                              if (activity.id !== activeActivity.id)
+                                return activity;
+
+                              return {
+                                ...activity,
+                                numberOfTimesCorrectAnswerGiven:
+                                  activity.numberOfTimesCorrectAnswerGiven + 1,
+                              };
+                            }
+                          );
+
+                          return {
+                            ...section,
+                            activities: _updatedActivities,
+                          };
+                        }
+                      );
+
+                      return {
+                        ...sublevel,
+                        sections: _updatedSections,
+                      };
+                    });
+
+                    return {
+                      ...level,
+                      modules: _updatedModules,
+                    };
+                  });
+
+                  setIsUpdatingSession(true);
+                  router.push({
+                    pathname: "/modal",
+                    params: { correctOption: option.title },
+                  });
+                  setTimeout(() => {
+                    setIsUpdatingSession(false);
+                    updateLevels(_updatedLevels);
+                    setTappedAnswer(undefined);
+                    initNextActivity();
+                    router.back();
+                  }, 1000);
+                } else {
+                  setIsUpdatingSession(true);
+                  setTimeout(() => {
+                    setIsUpdatingSession(false);
+                    setTappedAnswer(undefined);
+                    initNextActivity();
+                  }, 1000);
+                  console.log(activeActivity);
+                  console.log({ tappedAnswer });
+                  ``;
+                  console.log(option.id, activeActivity.correctAnswer.id);
+                  console.log("wrong answer");
+                }
+              }}
+              className={clsx(
+                "bg-colors-purple-200 w-24  h-24 rounded-full flex items-center justify-center absolute",
+                {
+                  "left-10 top-40": index === 0,
+                  "left-36 top-72": index === 1,
+                  "right-0 top-40": index === 2,
+                  " bg-red-500 text-white":
+                    isUpdatingSession &&
+                    option.id === tappedAnswer?.id &&
+                    activeActivity.correctAnswer.id !== tappedAnswer.id,
+                  "text-green-400":
+                    isUpdatingSession &&
+                    option.id === tappedAnswer?.id &&
+                    activeActivity.correctAnswer.id === tappedAnswer.id,
+                }
+              )}
+            >
+              <Text
+                className={clsx("text-4xl text-colors-purple-500  font-bold", {
+                  "  text-white":
+                    isUpdatingSession &&
+                    option.id === tappedAnswer?.id &&
+                    activeActivity.correctAnswer.id !== tappedAnswer.id,
+                })}
+              >
+                {option.title}
+              </Text>
+            </Pressable>
+          ))}
+        </View>
       </View>
       <DynamicModal ref={dynamicModalRef}>
         <View className="p-4 bg-white rounded-lg">
-          <Text>Letter Name Activity</Text>
+          <Text>Letter name activity</Text>
           <View className="h-20 flex items-center justify-center">
-            <NameIcon />
+            <EarIcon />
           </View>
           <Text className="mt-4">
             Start your language learning adventure. Let A, B, C, and D be the
