34205f703d9df2469137e7df016fe163f167b961
vaniilajs <vaniillajs@gmail.com>
2024-08-03 11:11:14 +0530

    refactor: add React Native DND library and its components and types

---
+++ src/vendor/react-native-dnd/hooks/useLatestValue.ts
@@ -0,0 +1,18 @@
+import type { DependencyList } from "react";
+import { useLayoutEffect, useRef } from "react";
+
+export function useLatestValue<T>(
+  value: T,
+  dependencies: DependencyList = [value]
+) {
+  const valueRef = useRef<T>(value);
+
+  useLayoutEffect(() => {
+    if (valueRef.current !== value) {
+      valueRef.current = value;
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, dependencies);
+
+  return valueRef;
+}
---
+++ src/vendor/react-native-dnd/types/reanimated.ts
@@ -0,0 +1,3 @@
+import type { useAnimatedReaction } from "react-native-reanimated";
+
+export type DependencyList = Parameters<typeof useAnimatedReaction>[2];
