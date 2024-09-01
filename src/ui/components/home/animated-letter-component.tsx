9a451e88e3697f2d58982be8d72a0c8e65c3fc86
vaniilajs <vaniillajs@gmail.com>
2024-09-01 18:12:11 +0530

    fix: overflow hidden added for letter introduction screen to enable header button on press handler

--- src/ui/components/home/animated-letter-component.tsx
+++ src/ui/components/home/animated-letter-component.tsx
@@ -68,7 +68,7 @@ const AnimatedLetterComponent = forwardRef<
 
   return (
     <View className="z-100 absolute flex size-full flex-row justify-around  border-purple-500">
-      <View className="flex w-[90px] items-center justify-center">
+      <View className="mb-12 flex w-[90px] items-center justify-center">
         {renderLowercaseLetter()}
       </View>
       <View className="mb-12 flex w-[80px] items-center justify-center">
