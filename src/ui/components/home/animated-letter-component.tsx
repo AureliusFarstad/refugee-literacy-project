80a8a4c5a048ca5850289d61e261e44aa2c400f8
vaniilajs <vaniillajs@gmail.com>
2024-09-02 17:36:55 +0530

    chore: on animation complete functionality added with reusable animation enabled characters

--- src/ui/components/home/animated-letter-component.tsx
+++ src/ui/components/home/animated-letter-component.tsx
@@ -1,11 +1,13 @@
-import LowerA from "assets/alphabet/animation/lowercase/a-pencil-formation-12px";
-import UpperA from "assets/alphabet/animation/uppercase/a-pencil-formation-12px";
+import LowercaseLetter from "assets/alphabet/animation/lowercase/lowercase-letter";
+import UppercaseLetter from "assets/alphabet/animation/uppercase/uppercase-letter";
 import React, { forwardRef, useImperativeHandle, useRef } from "react";
 import { View } from "react-native";
 
 type AnimatedLetterComponentProps = {
   name: string;
   onAnimationComplete: (letter: string) => void;
+  onAnimationStart: () => void;
+  isAnimating: boolean;
 };
 
 type AnimatedLetterComponentRef = {
@@ -13,61 +15,65 @@ type AnimatedLetterComponentRef = {
   animateUppercase: () => void;
 };
 
+interface LetterRef {
+  play?: () => void;
+}
+
 const AnimatedLetterComponent = forwardRef<
   AnimatedLetterComponentRef,
   AnimatedLetterComponentProps
->(({ name, onAnimationComplete }, ref) => {
-  const lowercaseWebView = useRef(null);
-  const uppercaseWebView = useRef(null);
+>(({ name, onAnimationComplete, onAnimationStart, isAnimating }, ref) => {
+  const lowercaseRef = useRef<LetterRef>(null);
+  const uppercaseRef = useRef<LetterRef>(null);
+
   const animateLowercase = () => {
-    const jsCommand = `document.querySelector('svg').svgatorPlayer['ready']((player) => player.play()); true;`;
-    // @ts-ignore
-    lowercaseWebView?.current?.injectJavaScript(jsCommand);
+    if (!isAnimating && lowercaseRef.current && lowercaseRef.current.play) {
+      onAnimationStart();
+      lowercaseRef.current.play();
+    }
   };
 
   const animateUppercase = () => {
-    const jsCommand = `document.querySelector('svg').svgatorPlayer['ready']((player) => player.play()); true;`;
-    // @ts-ignore
-    uppercaseWebView?.current?.injectJavaScript(jsCommand);
+    if (!isAnimating && uppercaseRef.current && uppercaseRef.current.play) {
+      onAnimationStart();
+      uppercaseRef.current.play();
+    }
   };
 
-  useImperativeHandle(ref, () => {
-    return {
-      animateLowercase: () => {
-        animateLowercase();
-        onAnimationComplete(name);
-      },
-      animateUppercase: () => {
-        animateUppercase();
-        onAnimationComplete(name.toUpperCase());
-      },
-    };
-  }, [name, onAnimationComplete]);
+  useImperativeHandle(ref, () => ({
+    animateLowercase,
+    animateUppercase,
+  }));
+
+  const handleLowercaseComplete = () => {
+    onAnimationComplete(name);
+  };
+
+  const handleUppercaseComplete = () => {
+    onAnimationComplete(name.toUpperCase());
+  };
+
+  const lowerCaseLetterSvgProps = {
+    onAnimationComplete: handleLowercaseComplete,
+    letter: name,
+  };
+  const uppercaseCaseLetterSvgProps = {
+    onAnimationComplete: handleUppercaseComplete,
+    letter: name.toUpperCase(),
+  };
 
   const renderLowercaseLetter = () => {
-    switch (name) {
-      case "a":
-        return <LowerA ref={lowercaseWebView} key={name} />;
-      case "s":
-        return <LowerA ref={lowercaseWebView} key={name} />;
-      default:
-        return <LowerA ref={lowercaseWebView} key={name} />;
-    }
+    return <LowercaseLetter ref={lowercaseRef} {...lowerCaseLetterSvgProps} />;
   };
 
   const renderUppercaseLetter = () => {
-    switch (name.toUpperCase()) {
-      case "A":
-        return <UpperA ref={uppercaseWebView} key={name.toUpperCase()} />;
-      case "S":
-        return <UpperA ref={uppercaseWebView} key={name.toUpperCase()} />;
-      default:
-        return <UpperA ref={uppercaseWebView} key={name.toUpperCase()} />;
-    }
+    return (
+      <UppercaseLetter ref={uppercaseRef} {...uppercaseCaseLetterSvgProps} />
+    );
   };
 
   return (
-    <View className="z-100 absolute flex size-full flex-row justify-around  border-purple-500">
+    <View className="z-100 absolute flex size-full flex-row justify-around border-purple-500">
       <View className="mb-12 flex w-[90px] items-center justify-center">
         {renderLowercaseLetter()}
       </View>
