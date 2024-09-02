86965e067df1cb203032916be3f3685750c2c27f
vaniilajs <vaniillajs@gmail.com>
2024-09-02 17:34:31 +0530

    refactor: remove unused files related to react-native-dnd library

--- src/vendor/react-native-dnd/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./components";
-export * from "./DndContext";
-export * from "./DndProvider";
-export * from "./features";
-export * from "./hooks";
-export * from "./types";
-export * from "./utils";
--- src/vendor/react-native-dnd/types/reanimated.ts
+++
@@ -1,3 +0,0 @@
-import type { useAnimatedReaction } from "react-native-reanimated";
-
-export type DependencyList = Parameters<typeof useAnimatedReaction>[2];
