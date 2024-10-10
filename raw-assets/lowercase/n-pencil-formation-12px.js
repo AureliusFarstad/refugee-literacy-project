d042902083d6e650c3dc82939e28925a8cd2556d
marcusfarstad@protonmail.ch <marcusfarstad@protonmail.ch>
2024-10-10 20:11:38 +0200

    chore: copy existing letter assets to raw

---
+++ raw-assets/lowercase/n-pencil-formation-12px.js
@@ -0,0 +1,46 @@
+import SVGatorPlayer from "@svgator/react-native";
+import React from "react";
+import WebView from "react-native-webview";
+
+function getHtml() {
+  SVGatorPlayer.wrapPage(
+    '<svg id="ekcCBdi1ADu1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 -100 164 400" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" project-id="3ac4f83ab2b34d6aaaea667a6f32aa5e" export-id="76079a4c9bfa430bb603cc5e5b029346" cached="false"><path d="M7.81,105.44c-1.6,28.71-5.41,69.58-1.84,94.48.16-23.68-3.88-65.84,12.72-83.63c10.13-10.86,24.9-10.27,35.94-2.77c13.46,9.15,11.64,23.43,12.32,37.87.8,16.82-.88,32.57-3.55,48.3" transform="translate(4 0)" fill="none" stroke="#d9d9d9" stroke-width="12" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"/><path id="ekcCBdi1ADu3" d="M7.81,105.44c-1.6,28.71-5.41,69.58-1.84,94.48.16-23.68-3.88-65.84,12.72-83.63c10.13-10.86,24.9-10.27,35.94-2.77c13.46,9.15,11.64,23.43,12.32,37.87.8,16.82-.88,32.57-3.55,48.3" transform="translate(4 0)" fill="none" stroke="#3f3f46" stroke-width="12" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-dasharray="312"/><g id="ekcCBdi1ADu4" transform="matrix(2.108072 0 0 2.108072 2.808073 8.99274)" opacity="0"><path d="M3.87809,47.8096c-.32,0-.67-.1-.98-.37-.26-.23-.44-.54-.5-.88-.04-.23-.02-.42,0-.52.01-.1.04-.21.06-.27.03-.1.08-.25.14-.42.11-.31.28-.76.48-1.28.4-1.04.95-2.43,1.5-3.83.55-1.39,1.11-2.78,1.53-3.83l.97-2.41.17.06l1.56-1.55L39.9681,1.34961c1.49-1.500001,4.1-1.500001,5.6,0l3.06,3.07c1.54,1.54,1.54,4.06,0,5.59999l-32.57,32.59c0,0-.07.07-.11.11l.05.11-.52.26c0,0-.02.01-.04.02l-.58.25-4.90001,2.05c-5.4,2.26-5.52,2.3-5.57,2.32-.15.05-.32.09-.51.09v-.01Zm4.28-12.47l-.66,1.63c-.42,1.04-.98,2.43-1.53,3.82s-1.1,2.77-1.49,3.81c-.2.51-.36.94-.46,1.25-.05.14-.09.25-.11.34l-.02.11c.09-.03.92-.37,5.51-2.29l5.36001-2.24c0,0,.06-.04.09-.07.04-.04.08-.08.1-.1l.08-.09l32.54-32.54999c.98-.98.98-2.5,0-3.48l-3.06-3.07c-.93-.93-2.55-.93-3.48,0L8.15809,35.3396Z" fill="#3f3f46"/><path d="M13.8881,42.4804c-.1-1.08-.25-2.3401-.36-2.9701-.32-.02-.77-.0399-1.08-.0599-1.31-.06-1.39-.07-1.61-.24-.32-.24-.32-.31-.47-2.19-.02-.27-.04-.54-.06-.75-.64997-.1-1.86997-.2701-2.97998-.4201l.19001-1.4899c3.81997.49,3.83997.52,4.06997.84.16.22.16.27.27,1.7.02.31.05.7.08,1.01.17,0,.37.0199.57.0299c1.48.07,1.71.1001,1.92.2001.4.2.56.28.93,4.18l-1.49.14.02.02Z" fill="#3f3f46"/><path d="M37.346,5.35945L36.2852,6.41992l7.593,7.59568l1.0608-1.0605-7.593-7.59565Z" fill="#3f3f46"/><path d="M38.7778,7.44873L10.8047,35.4219l1.0606,1.0606L39.8385,8.50939L38.7778,7.44873Z" fill="#3f3f46"/><path d="M40.727,10.6909L12.7539,38.6641l1.0607,1.0606L41.7877,11.7516L40.727,10.6909Z" fill="#3f3f46"/><path d="M7.63813,45.15c0,0-.33-1.15-1.04-1.77-.74-.65-1.76001-.93-1.77001-.93L5.20813,41c.06.01,1.36.36,2.38,1.25c1.05.92,1.47,2.44,1.48999,2.5l-1.44999.4h.01Z" fill="#3f3f46"/><circle style="isolation:isolate" r="2.21301" transform="matrix(1.339778 0 0 1.339778 4.362875 45.844065)" fill="#c385f8"/></g>\r\n<script><![CDATA[\r\n' +
+      SVGatorPlayer.getPlayer("91c80d77") +
+      '(function(s,i,o,w,d,a,b){(a=Array.from(d.querySelectorAll(\'svg#\' + i.root)).filter(n=> !n.svgatorPlayer)[0]||{}).svgatorPlayer={ready:(function(a){b=[];return function(c){return c?(b.push(c),a.svgatorPlayer):b}})(a)};w[o]=w[o]||{};w[o][s]=w[o][s]||[];w[o][s].push(i);})(\'91c80d77\',{"root":"ekcCBdi1ADu1","version":"2022-05-04","animations":[{"elements":{"ekcCBdi1ADu3":{"stroke-dashoffset":[{"t":0,"v":0,"e":[1,1]},{"t":100,"v":312},{"t":5100,"v":0}]},"ekcCBdi1ADu4":{"transform":{"data":{"s":{"x":2.108072,"y":2.108072},"t":{"x":-4.270218,"y":-45.751407}},"keys":{"o":[{"t":100,"v":{"x":11.81,"y":105.44,"type":"cusp","end":{"x":10.21,"y":134.15}}},{"t":1620,"v":{"x":9.97,"y":199.92,"type":"cusp","start":{"x":6.4,"y":175.02},"end":{"x":10.13,"y":176.24}}},{"t":3010,"v":{"x":22.69,"y":116.29,"type":"cusp","start":{"x":6.09,"y":134.08},"end":{"x":32.82,"y":105.43}}},{"t":3650,"v":{"x":58.63,"y":113.52,"type":"cusp","start":{"x":47.59,"y":106.02},"end":{"x":72.09,"y":122.67}}},{"t":4320,"v":{"x":70.95,"y":151.39,"type":"cusp","start":{"x":70.27,"y":136.95},"end":{"x":71.75,"y":168.21}}},{"t":5100,"v":{"x":67.4,"y":199.69,"type":"cusp","start":{"x":70.07,"y":183.96}}}]}},"opacity":[{"t":0,"v":0},{"t":100,"v":1}]}},"s":"MDNA1ZGI3NWVhMWGIxYWU5ZGIwUYTVhYlRhYTVJlUDc2NzE2ZDYZjQzZjNjg1ZAWEwYTVhZWExLOWZiMGE1RmFAiYWE1ZTc2NmGQ2ODVlT2E1YLjBhMU1hZTlkLR2IwYTVhYmFPhYWY1ZTc2NmPRJNjg1ZU5hMUmE1YThhODVlJRDc2NmQ2ODVSlOWRhOGIwUGUExYWVhYTlkYBjBhMTVlNzZhFMjlkYThhZmENxNjhWNWVhZmBFjYTFhMWEwNXWU3NjZkUDY4PTTVlYTJhY2FSmNWU3NjZkNmCM2Y2I5"}],"options":"MDJAxODhhMzE4MKjgzNzA4MTgzMMzE0OTMxUDdKmODE3ZTc2ODLE3MDdjN2M3MCFE4Mzc4NzIzBMThj"},\'__SVGATOR_PLAYER__\',window,document)\r\n]]></script>\r\n</svg>\r\n',
+  );
+}
+
+const SVGatorComponent = React.forwardRef((props, ref) => {
+  const html = getHtml();
+  if (!SVGatorPlayer.getWebViewProps) {
+    console.warn(
+      "Your currently installed @svgator/react-native package is outdated. " +
+        "Please update it to the newest version. " +
+        "See more: https://www.npmjs.com/package/@svgator/react-native",
+    );
+    const newProps = SVGatorPlayer.parseProps(props, html);
+    return (
+      <WebView
+        ref={ref}
+        {...newProps}
+        source={{ html }}
+        containerStyle={{ flex: 0 }}
+        style={{ backgroundColor: "transparent", flex: 0 }}
+      />
+    );
+  }
+
+  const { newProps, styles } = SVGatorPlayer.getWebViewProps(props, html);
+
+  return (
+    <WebView
+      ref={ref}
+      {...newProps}
+      source={{ html }}
+      containerStyle={styles.container}
+      style={styles.style}
+    />
+  );
+});
+
+export default SVGatorComponent;
